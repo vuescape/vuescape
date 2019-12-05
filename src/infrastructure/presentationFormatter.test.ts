da92038e41bcfc55dfd51355d2270bd788b8f91b
randylevy <randy.levy@gmail.com>
2019-12-05 17:47:50 -0500

    Add all presentation formats

---
+++ src/infrastructure/presentationFormatter.test.ts
@@ -0,0 +1,440 @@
+import expect from 'expect'
+import {
+  DEFAULT_EMPTY_DISPLAY,
+  formatValue,
+  round,
+  toDateDisplay,
+  toDateTimeDisplay,
+  toTextDisplay,
+} from './presentationFormatter'
+
+describe('presentationFormatter --', () => {
+  describe('toTextDisplay --', () => {
+    it('should return default emptyDisplay when value is not a string', () => {
+      // Arrange
+      const inputValue = {}
+
+      // Act
+      const displayValue = toTextDisplay(inputValue)
+
+      // Assert
+      expect(displayValue).toBe(DEFAULT_EMPTY_DISPLAY)
+    })
+
+    it('should return emptyDisplay when value is not a string', () => {
+      // Arrange
+      const inputValue = {}
+      const emptyDisplay = 'not a string'
+
+      // Act
+      const displayValue = toTextDisplay(inputValue, emptyDisplay)
+
+      // Assert
+      expect(displayValue).toBe(emptyDisplay)
+    })
+
+    it('should return emptyDisplay when value is a zero length string', () => {
+      // Arrange
+      const inputValue = ''
+
+      // Act
+      const displayValue = toTextDisplay(inputValue)
+
+      // Assert
+      expect(displayValue).toBe(DEFAULT_EMPTY_DISPLAY)
+    })
+
+    it('should return original value when value is a non-zero length string', () => {
+      // Arrange
+      const inputValue = 'String to Display'
+
+      // Act
+      const displayValue = toTextDisplay(inputValue)
+
+      // Assert
+      expect(displayValue).toBe(inputValue)
+    })
+  })
+
+  describe('toDateTimeDisplay --', () => {
+    it('should return default emptyDisplay when value is not a string', () => {
+      // Arrange
+      const inputValue = {}
+
+      // Act
+      const displayValue = toDateTimeDisplay(inputValue)
+
+      // Assert
+      expect(displayValue).toBe(DEFAULT_EMPTY_DISPLAY)
+    })
+
+    it('should return emptyDisplay when value is not a string', () => {
+      // Arrange
+      const inputValue = {}
+      const emptyDisplay = 'not a string'
+
+      // Act
+      const displayValue = toDateTimeDisplay(inputValue, emptyDisplay)
+
+      // Assert
+      expect(displayValue).toBe(emptyDisplay)
+    })
+
+    it('should return emptyDisplay when value is a zero length string', () => {
+      // Arrange
+      const inputValue = ''
+
+      // Act
+      const displayValue = toDateTimeDisplay(inputValue)
+
+      // Assert
+      expect(displayValue).toBe(DEFAULT_EMPTY_DISPLAY)
+    })
+
+    it('should return emptyDisplay value when value is a not a valid datetime', () => {
+      // Arrange
+      const inputValue = 'Invalid/date/time'
+
+      // Act
+      const displayValue = toDateTimeDisplay(inputValue)
+
+      // Assert
+      expect(displayValue).toBe(DEFAULT_EMPTY_DISPLAY)
+    })
+
+    it('should return formatted datetime when value is a valid datetime in the pm', () => {
+      // Arrange
+      const inputValue = '2019/01/31 22:00:00'
+
+      // Act
+      const displayValue = toDateTimeDisplay(inputValue)
+
+      // Assert
+      expect(displayValue).toBe('1/31/2019 10:00 pm')
+    })
+
+    it('should return formatted datetime when value is a valid datetime in the am', () => {
+      // Arrange
+      const inputValue = '2019/01/31 10:00:00'
+
+      // Act
+      const displayValue = toDateTimeDisplay(inputValue)
+
+      // Assert
+      expect(displayValue).toBe('1/31/2019 10:00 am')
+    })
+  })
+
+  describe('toDateDisplay --', () => {
+    it('should return default emptyDisplay when value is not a string', () => {
+      // Arrange
+      const inputValue = {}
+
+      // Act
+      const displayValue = toDateDisplay(inputValue)
+
+      // Assert
+      expect(displayValue).toBe(DEFAULT_EMPTY_DISPLAY)
+    })
+
+    it('should return emptyDisplay when value is not a string', () => {
+      // Arrange
+      const inputValue = {}
+      const emptyDisplay = 'not a string'
+
+      // Act
+      const displayValue = toDateDisplay(inputValue, emptyDisplay)
+
+      // Assert
+      expect(displayValue).toBe(emptyDisplay)
+    })
+
+    it('should return emptyDisplay when value is a zero length string', () => {
+      // Arrange
+      const inputValue = ''
+
+      // Act
+      const displayValue = toDateDisplay(inputValue)
+
+      // Assert
+      expect(displayValue).toBe(DEFAULT_EMPTY_DISPLAY)
+    })
+
+    it('should return emptyDisplay value when value is a not a valid date', () => {
+      // Arrange
+      const inputValue = 'Invalid/date'
+
+      // Act
+      const displayValue = toDateDisplay(inputValue)
+
+      // Assert
+      expect(displayValue).toBe(DEFAULT_EMPTY_DISPLAY)
+    })
+
+    it('should return formatted date when value is a valid date', () => {
+      // Arrange
+      const inputValue = '2019/01/31'
+
+      // Act
+      const displayValue = toDateDisplay(inputValue)
+
+      // Assert
+      expect(displayValue).toBe('1/31/2019')
+    })
+  })
+
+  describe('round --', () => {
+    it('should use 0 decimal places as default', () => {
+      // Arrange
+      const inputValue = 22.1
+
+      // Act
+      const roundedValue = round(inputValue)
+
+      // Assert
+      expect(roundedValue).toEqual(22)
+    })
+
+    it('should round to 1 decimal place when specified', () => {
+      // Arrange
+      const inputValue = 22.123
+
+      // Act
+      const roundedValue = round(inputValue, 1)
+
+      // Assert
+      expect(roundedValue).toEqual(22.1)
+    })
+
+    it('should round up to the nearest number and respect number of decimal places', () => {
+      // Arrange
+      const inputValue = 22.555
+
+      // Act
+      const roundedValue = round(inputValue, 0)
+
+      // Assert
+      expect(roundedValue).toEqual(23)
+    })
+
+    it('should round up away from zero when input is negative', () => {
+      // Arrange
+      const inputValue = -22.555
+
+      // Act
+      const roundedValue = round(inputValue, 1)
+
+      // Assert
+      expect(roundedValue).toEqual(-22.6)
+    })
+  })
+
+  describe('formatValue --', () => {
+    describe('currency --', () => {
+      it('should format correctly for positive value', () => {
+        // Arrange
+        const presentationFormatKind = 'currency'
+        const inputValue = '22.55'
+        const numberOfDecimalPlaces = 1
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('$22.6')
+      })
+
+      it('should format correctly for negative value', () => {
+        // Arrange
+        const presentationFormatKind = 'currency'
+        const inputValue = '-22.55'
+        const numberOfDecimalPlaces = 1
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('-$22.6')
+      })
+
+      it('should format correctly for a small negative value', () => {
+        // Arrange
+        const presentationFormatKind = 'currency'
+        const inputValue = '-0.055'
+        const numberOfDecimalPlaces = 1
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('-$0.1')
+      })
+
+      it('should format correctly for zero', () => {
+        // Arrange
+        const presentationFormatKind = 'currency'
+        const inputValue = '-0.00009'
+        const numberOfDecimalPlaces = 2
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('$0.00')
+      })
+    })
+
+    describe('percentage --', () => {
+      it('should format correctly for positive value', () => {
+        // Arrange
+        const presentationFormatKind = 'percentage'
+        const inputValue = '22.555'
+        const numberOfDecimalPlaces = 2
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('22.56%')
+      })
+
+      it('should format correctly for negative value', () => {
+        // Arrange
+        const presentationFormatKind = 'percentage'
+        const inputValue = '-22.555'
+        const numberOfDecimalPlaces = 2
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('-22.56%')
+      })
+    })
+
+    describe('number --', () => {
+      it('should format correctly for positive value', () => {
+        // Arrange
+        const presentationFormatKind = 'number'
+        const inputValue = '22.555'
+        const numberOfDecimalPlaces = 2
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('22.56')
+      })
+
+      it('should format correctly for negative value', () => {
+        // Arrange
+        const presentationFormatKind = 'number'
+        const inputValue = '-22.555'
+        const numberOfDecimalPlaces = 2
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('-22.56')
+      })
+    })
+
+    describe('ratio --', () => {
+      it('should format correctly for positive value', () => {
+        // Arrange
+        const presentationFormatKind = 'number'
+        const inputValue = '22.555'
+        const numberOfDecimalPlaces = 2
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('22.56')
+      })
+
+      it('should format correctly for negative value', () => {
+        // Arrange
+        const presentationFormatKind = 'number'
+        const inputValue = '-22.555'
+        const numberOfDecimalPlaces = 2
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('-22.56')
+      })
+    })
+
+    describe('accounting --', () => {
+      it('should format correctly for positive value', () => {
+        // Arrange
+        const presentationFormatKind = 'accounting'
+        const inputValue = '22.555'
+        const numberOfDecimalPlaces = 2
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('22.56')
+      })
+
+      it('should format correctly for large value', () => {
+        // Arrange
+        const presentationFormatKind = 'accounting'
+        const inputValue = '999999999.555'
+        const numberOfDecimalPlaces = 2
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('999,999,999.56')
+      })
+
+      it('should format correctly for negative value', () => {
+        // Arrange
+        const presentationFormatKind = 'accounting'
+        const inputValue = '-22.555'
+        const numberOfDecimalPlaces = 2
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('(22.56)')
+      })
+    })
+
+    describe('mmcurrency --', () => {
+      it('should format correctly for positive value', () => {
+        // Arrange
+        const presentationFormatKind = 'mmcurrency'
+        const inputValue = '151651622.555'
+        const numberOfDecimalPlaces = 2
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('$151,651,622.56mm')
+      })
+
+      it('should format correctly for negative value', () => {
+        // Arrange
+        const presentationFormatKind = 'mmcurrency'
+        const inputValue = '-151651622.555'
+        const numberOfDecimalPlaces = 2
+
+        // Act
+        const formattedValue = formatValue(presentationFormatKind, inputValue, numberOfDecimalPlaces)
+
+        // Assert
+        expect(formattedValue).toEqual('-$151,651,622.56mm')
+      })
+    })
+  })
+})
--- src/infrastructure/presentationFormatter.ts
+++ src/infrastructure/presentationFormatter.ts
@@ -1,13 +1,28 @@
 import { Dictionary } from '@vuescape/types'
 
-export function round(value: any, places: number = 0) {
-  let p = places || 0
-  p = p < 0 ? 0 : p
+export const DEFAULT_EMPTY_DISPLAY = '-'
 
-  const factor = Math.pow(10, p)
-  const rounded = Math.round(value * factor) / factor
+/**
+ * @callback formatResult
+ * @param {number} value - The value to format for display.
+ * @returns {string} - The formatted result value.
+ */
 
-  return rounded
+/**
+ * Validates data and converts it into a number, then calls formatResult with the number to get the formated result.
+ * @param value - The value to attempt to convert into a number.
+ * @param {formatResult} formatResult - The function that will format the result for valid numbers.
+ * @param emptyDisplay - The value to return if the input value is empty.
+ * @returns {string} - The formatted result value from formatResult or an empty result.
+ */
+const formatWithNumber = (value: string, formatResult: (value: number) => string, emptyDisplay: string): string => {
+  if (value == null || value === '') {
+    return emptyDisplay
+  }
+
+  const numberValue = Number(value)
+
+  return isFinite(numberValue) ? formatResult(numberValue) : emptyDisplay
 }
 
 const toNumber = (value: any, places: number) => {
@@ -15,7 +30,7 @@ const toNumber = (value: any, places: number) => {
   p = p < 0 ? 0 : p
 
   const factor = Math.pow(10, p)
-  const rounded = Math.abs(Math.round(value * factor)) / factor
+  const rounded = Math.round(Math.abs(value * factor)) / factor
   const parts = (rounded + '').split('.')
 
   let result = parts[0]
@@ -34,32 +49,134 @@ const toNumber = (value: any, places: number) => {
   return result
 }
 
-const toAccountingDisplay = (value: any, places: number) => {
+const toAccountingDisplay = (value: string, places: number, emptyDisplay: string) => {
   const p = places || 1
-  return round(value, p) >= 0.0 ? toNumber(value, p) : '(' + toNumber(value, p) + ')'
+  const formatter = (numberValue: number) =>
+    round(numberValue, p) >= 0.0 ? toNumber(numberValue, p) : '(' + toNumber(numberValue, p) + ')'
+  return formatWithNumber(value, formatter, emptyDisplay)
 }
 
-const toCurrencyDisplay = (value: any, places: number) => {
-  return round(value) >= 0.0 ? '$' + toNumber(value, places) : '-$' + toNumber(value, places)
+const toCurrencyDisplay = (value: string, places: number, emptyDisplay: string) => {
+  const formatter = (numberValue: number) =>
+    round(numberValue, places) >= 0.0 ? '$' + toNumber(numberValue, places) : '-$' + toNumber(numberValue, places)
+
+  return formatWithNumber(value, formatter, emptyDisplay)
 }
 
-const toPercentageDisplay = (value: any, places: number) => {
-  // TODO: do this better
-  value *= 100
-  return round(value, places) >= 0.0 ? toNumber(value, places) + '%' : '-' + toNumber(value, places) + '%'
+const toMMCurrencyDisplay = (value: string, places: number, emptyDisplay: string) => {
+  const formatter = (numberValue: number) => {
+    const p = places || 1
+    return round(numberValue, p) >= 0.0
+      ? '$' + toNumber(numberValue, p) + 'mm'
+      : '-$' + toNumber(numberValue, p) + 'mm'
+  }
+
+  return formatWithNumber(value, formatter, emptyDisplay)
 }
 
-const toNumberDisplay = (value: any, places: number) => {
-  return round(value, places) >= 0.0 ? toNumber(value, places) : '-' + toNumber(value, places)
+const toPercentageDisplay = (value: string, places: number, emptyDisplay: string) => {
+  const formatter = (numberValue: number) =>
+    round(numberValue, places) >= 0.0 ? toNumber(numberValue, places) + '%' : '-' + toNumber(numberValue, places) + '%'
+
+  return formatWithNumber(value, formatter, emptyDisplay)
 }
 
-const presentationFormatMap: Dictionary<(value: any, places: number) => string> = {
+const toNumberDisplay = (value: string, places: number, emptyDisplay: string) => {
+  const formatter = (numberValue: number) =>
+    round(numberValue, places) >= 0.0 ? toNumber(numberValue, places) : '-' + toNumber(numberValue, places)
+
+  return formatWithNumber(value, formatter, emptyDisplay)
+}
+
+export const toDateDisplay = (value: any, emptyValue = '-') => {
+  if (value == null) {
+    return emptyValue
+  }
+
+  let dateValue = new Date(value)
+  // This is to fix a Safari problem in its handling of UTC dates
+  if (isNaN(dateValue.getTime())) {
+    dateValue = new Date(value + 'Z')
+  }
+  value = dateValue
+
+  const result = isNaN(dateValue.getTime())
+    ? emptyValue
+    : value.getMonth() + 1 + '/' + value.getDate() + '/' + value.getFullYear()
+
+  return result
+}
+
+export const toDateTimeDisplay = (value: any, emptyValue = '-') => {
+  if (value == null) {
+    return emptyValue
+  }
+
+  let dateValue = new Date(value)
+  // This is to fix a Safari problem in its handling of UTC dates
+  dateValue = isNaN(dateValue.getTime()) ? new Date(dateValue + 'Z') : dateValue
+
+  let hours = dateValue.getHours()
+  const amPm = hours < 12 ? 'am' : 'pm'
+  hours = hours < 12 ? hours : hours - 12
+  hours = hours === 0 ? 12 : hours
+  let minutes = '' + dateValue.getMinutes()
+  minutes = minutes.length === 1 ? '0' + minutes : minutes
+  // let seconds = '' + value.getSeconds();
+  // seconds = seconds.length === 1 ? '0' + seconds : seconds;
+
+  return isNaN(dateValue.getTime())
+    ? emptyValue
+    : dateValue.getMonth() +
+        1 +
+        '/' +
+        dateValue.getDate() +
+        '/' +
+        dateValue.getFullYear() +
+        ' ' +
+        hours +
+        ':' +
+        minutes +
+        // + ":" + seconds
+        ' ' +
+        amPm
+}
+
+export const toTextDisplay = (value: any, emptyDisplay = '-') => {
+  if (typeof value === 'string' && value.length > 0) {
+    return value
+  }
+
+  return emptyDisplay
+}
+
+export type presentationFormatKind = 'currency' | 'percentage' | 'number' | 'ratio' | 'accounting' | 'mmcurrency'
+
+export const presentationFormatMap: Dictionary<(value: any, places: number, emptyDisplay: string) => string> = {
   currency: toCurrencyDisplay,
   percentage: toPercentageDisplay,
   number: toNumberDisplay,
+  ratio: toNumberDisplay,
+  accounting: toAccountingDisplay,
+  mmcurrency: toMMCurrencyDisplay,
 }
 
-export function formatValue(presentationFormat: string, value: string, numberOfDecimalPlaces: number) {
+export function round(value: number, places: number = 0) {
+  let p = places || 0
+  p = p < 0 ? 0 : p
+
+  const factor = Math.pow(10, p)
+  const rounded = (Math.sign(value) * Math.round(Math.abs(value * factor))) / factor
+
+  return rounded
+}
+
+export function formatValue(
+  presentationFormat: presentationFormatKind,
+  value: string,
+  numberOfDecimalPlaces: number,
+  emptyDisplay = DEFAULT_EMPTY_DISPLAY,
+) {
   const formatter = presentationFormatMap[presentationFormat]
   if (!formatter) {
     console.warn(
@@ -68,5 +185,5 @@ export function formatValue(presentationFormat: string, value: string, numberOfD
     return value
   }
 
-  return formatter(value, numberOfDecimalPlaces)
+  return formatter(value, numberOfDecimalPlaces, emptyDisplay)
 }
