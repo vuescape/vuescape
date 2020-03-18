f2e9d419e609888564779863c768918073ceda1e
randylevy <randy.levy@gmail.com>
2020-03-18 00:31:50 -0400

    Add LegendTable

---
+++ src/components/LegendTable/LegendTable.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <v-menu
+      :close-on-content-click="false"
+      offset-x
+      nudge-left="315"
+      origin="center right"
+      transition="scale-transition"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" outline depressed v-on="on" class="legend-table__v-btn--style">
+          <font-awesome-icon :icon="['fas', 'info']" class="legend-table__font-awesome-icon--style" />
+          <!-- <v-icon>fas fa-info</v-icon> -->
+        </v-btn>
+      </template>
+
+      <div class="legend-table__div--spaced">
+        <table class="legend-table__table--legend benchmarking">
+          <thead>
+            <tr>
+              <td>&nbsp;</td>
+              <td>Legend</td>
+            </tr>
+          </thead>
+          <tbody>
+            <slot></slot>
+          </tbody>
+        </table>
+      </div>
+    </v-menu>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component({})
+export default class LegendTable extends Vue {}
+</script>
+
+<style>
+.legend-table__div--spaced {
+  background: white;
+  padding: 10px;
+}
+.coscore__span--icon-button {
+  border: 1px solid #dddddd;
+  color: #dddddd;
+  border-radius: 5px;
+  height: 24px;
+  margin-top: 6px;
+  margin-right: 6px;
+}
+.legend-table__font-awesome-icon--style {
+  color: #bbbbbb;
+  font-size: 12px;
+  margin-right: -1px;
+}
+
+.legend-table__v-btn--style {
+  border: 1px solid #dddddd !important;
+  border-radius: 5px;
+  height: 24px;
+  width: 24px;
+  padding-left: 8px;
+  padding-right: 8px;
+  margin-top: 6px;
+  margin-right: 6px;
+}
+
+table.legend-table__table--legend thead td {
+  border: 0px;
+  background-color: #16a5c6 !important;
+  color: #ffffff;
+  text-align: center;
+  height: 32px;
+  font-size: 13px;
+  font-weight: 500;
+  padding-right: 20px;
+  padding-left: 20px;
+  border-bottom-color: rgb(85, 85, 85);
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
+}
+table.legend-table__table--legend tbody td {
+  padding-left: 10px;
+  padding-right: 10px;
+}
+table.legend-table__table--legend {
+  border-collapse: collapse;
+  font-family: 'Segoe UI';
+  padding: 10px;
+}
+table.legend-table__table--legend tr {
+  height: 28px;
+  min-width: 100px;
+}
+table.legend-table__table--legend tr:last-child {
+  border-bottom-color: rgb(85, 85, 85);
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
+}
+table.legend-table__table--legend thead td {
+  font-size: 13px;
+  border: 0px;
+  background-color: #16a5c6 !important;
+  color: #ffffff;
+  text-align: center;
+  height: 32px;
+  font-weight: 500;
+  padding-right: 20px;
+  padding-left: 20px;
+  border-bottom-color: rgb(85, 85, 85);
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
+}
+table.legend-table__table--legend tbody td {
+  padding-left: 10px;
+  padding-right: 10px;
+  font-size: 13px;
+}
+</style>
