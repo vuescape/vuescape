629e2b1e788fa775c191adc81b30ba23a2bc9eee
randylevy <randy.levy@gmail.com>
2020-07-02 02:45:01 -0400

    Temporarily enable hover over tooltip with Lorem Ipsum

--- src/components/TreeTable/FixedCellRenderer.vue
+++ src/components/TreeTable/FixedCellRenderer.vue
@@ -1,20 +1,28 @@
 <template>
-  <v-tooltip right>
-    <template v-slot:activator="{ on }">
-      <span
-        :class="cellToDisplay.cssClasses" 
-      >{{cellToDisplay.value}}</span>
-      <span v-if="false" v-on="on"></span> <!-- temp activator that is not shown to disable tooltip -->
+  <v-tooltip right hover>
+    <template max-width="200" v-slot:activator="{ on }">
+      <span :class="cellToDisplay.cssClasses">{{ cellToDisplay.value }} &nbsp;</span>
+      <span v-on="on">
+        <font-awesome-icon style="font-size: 16px; color: #0F738A;" :icon="['fal', 'question-circle']" />
+      </span>
+      <!-- temp activator that is not shown to disable tooltip -->
     </template>
-    <v-card>
-      <v-card-title primary-title>{{cellToDisplay.value}}</v-card-title>
-      <v-divider light></v-divider>
-      <span>{{cellToDisplay.title}}</span>
+    <v-card flat class="fixed-cell-renderer__card--hover">
+      <v-card-title>
+        <span class="title font-weight-light">{{cellToDisplay.value}}</span><v-spacer></v-spacer>
+      </v-card-title>
+      <v-card-text>
+        {{ cellToDisplay.value }} can be described as follows: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </v-card-text>
     </v-card>
   </v-tooltip>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import Component from 'vue-class-component'
 import { Prop } from 'vue-property-decorator'
 
@@ -32,3 +40,8 @@ export default class FixedCellRenderer extends ComponentBase {
   }
 }
 </script>
+<style>
+.fixed-cell-renderer__card--hover {
+  max-width: 300px;
+}
+</style>
