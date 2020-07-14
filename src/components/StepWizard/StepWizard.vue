d38ec2aeac750fceab2a082c9d943f79df56039c
randylevy <randy.levy@gmail.com>
2020-07-14 11:56:54 -0400

    remove mismatched comment tag

--- src/components/StepWizard/StepWizard.vue
+++ src/components/StepWizard/StepWizard.vue
@@ -286,7 +286,7 @@ export default class StepWizard extends Vue {
 .step-wizard__animation-slide-out {
   transition: opacity 0.3s ease-out;
 }
-*/ .step-wizard__stepper-box {
+.step-wizard__stepper-box {
   background-color: white;
   /* box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24); */
   min-height: 200px;
