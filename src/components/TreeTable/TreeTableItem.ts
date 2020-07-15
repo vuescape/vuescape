3e0f1abab48aac7e71d85999f0e30c7d68931eb8
randylevy <randy.levy@gmail.com>
2020-07-15 01:05:51 -0400

    Remove title.  Add hover and extendedProperties

--- src/components/TreeTable/TreeTableItem.ts
+++ src/components/TreeTable/TreeTableItem.ts
@@ -1,14 +1,14 @@
 import { TreeTableRow } from './TreeTableRow'
 
 export interface TreeTableItem {
-
   id: string
   // TODO: should value just be a string? or should it be really be any?
-  value: any 
-  title?: string
+  value: any
+  hover?: { title?: string; text?: string; html?: string }
   renderer?: string
   cssClasses?: string
   colspan?: number
   // Use this for clicking expand/collapse
   onclick?: (row?: TreeTableRow, item?: TreeTableItem) => void
+  extendedProperties?: any
 }
