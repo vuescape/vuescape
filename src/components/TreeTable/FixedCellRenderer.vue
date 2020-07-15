3ab437e4fd3ff098f9013b490a7560508f96fa40
randylevy <randy.levy@gmail.com>
2020-07-15 01:07:09 -0400

    Refactor to use new hover property and to render either text or HTML for hover over

--- src/components/TreeTable/FixedCellRenderer.vue
+++ src/components/TreeTable/FixedCellRenderer.vue
@@ -1,25 +1,31 @@
 <template>
-  <v-tooltip right hover>
-    <template max-width="200" v-slot:activator="{ on }">
-      <span :class="cellToDisplay.cssClasses">{{ cellToDisplay.value }} &nbsp;</span>
-      <span v-on="on">
-        <font-awesome-icon style="font-size: 16px; color: #0F738A;" :icon="['fal', 'question-circle']" />
-      </span>
-      <!-- temp activator that is not shown to disable tooltip -->
-    </template>
-    <v-card flat class="fixed-cell-renderer__card--hover">
-      <v-card-title>
-        <span class="title font-weight-light">{{cellToDisplay.value}}</span><v-spacer></v-spacer>
-      </v-card-title>
-      <v-card-text>
-        {{ cellToDisplay.value }} can be described as follows: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-        sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </v-card-text>
-    </v-card>
-  </v-tooltip>
+  <span @mouseleave="isHover = false" @mouseover="isHover = true">
+    <v-tooltip right hover>
+      <template max-width="200" v-slot:activator="{ on }">
+        <span :class="cell.cssClasses">{{ cell.value }} &nbsp;</span>
+        <span v-on="on" style="vertical-align: text-top;">
+          <transition name="fixed-cell-renderer__animation" mode="out-in">
+            <font-awesome-icon
+              v-if="canHover && isHover"
+              style="font-size: 14px; color: #16a5c6;"
+              :icon="['fal', 'info-circle']"
+            />
+          </transition>
+        </span>
+        <!-- temp activator that is not shown to disable tooltip -->
+      </template>
+      <v-card flat class="fixed-cell-renderer__card--hover">
+        <v-card-title>
+          <span class="title font-weight-light">{{ hoverTitle }}</span
+          ><v-spacer></v-spacer>
+        </v-card-title>
+        <v-card-text>
+          <span v-if="hoverText">{{ hoverText }} </span>
+          <span v-else v-html="hoverHtml"></span>
+        </v-card-text>
+      </v-card>
+    </v-tooltip>
+  </span>
 </template>
 
 <script lang="ts">
@@ -35,13 +41,35 @@ export default class FixedCellRenderer extends ComponentBase {
   @Prop({ type: Object, required: true })
   private cell: TreeTableItem
 
-  private get cellToDisplay() {
-    return this.cell
+  private isHover = false
+
+  private get canHover() {
+    return this.cell.hover
+  }
+
+  private get hoverTitle() {
+    return this.cell.hover ? this.cell.hover.title : ''
+  }
+
+  private get hoverText() {
+    return this.cell.hover ? this.cell.hover.text : ''
+  }
+
+  private get hoverHtml() {
+    return this.cell.hover ? this.cell.hover.html : ''
   }
 }
 </script>
 <style>
 .fixed-cell-renderer__card--hover {
   max-width: 300px;
+}
+.fixed-cell-renderer__animation-enter-active,
+.fixed-cell-renderer__animation-leave-active {
+  transition: opacity 0.3s;
+}
+.fixed-cell-renderer__animation-enter,
+.fixed-cell-renderer__animation-leave-to {
+  opacity: 0;
 }
 </style>
