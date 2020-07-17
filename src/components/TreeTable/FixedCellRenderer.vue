3e1c6f34ab4de4d7171e63bee3f4cef911978557
randylevy <randy.levy@gmail.com>
2020-07-17 02:34:34 -0400

    Use Tooltip for hover over

--- src/components/TreeTable/FixedCellRenderer.vue
+++ src/components/TreeTable/FixedCellRenderer.vue
@@ -1,30 +1,12 @@
 <template>
-  <span @mouseleave="isHover = false" @mouseover="isHover = true">
-    <v-tooltip right hover>
-      <template max-width="200" v-slot:activator="{ on }">
-        <span :class="cell.cssClasses">{{ cell.value }} &nbsp;</span>
-        <span v-on="on" style="vertical-align: text-top;">
-          <transition name="fixed-cell-renderer__animation" mode="out-in">
-            <font-awesome-icon
-              v-if="canHover && isHover"
-              style="font-size: 14px; color: #16a5c6;"
-              :icon="['fal', 'info-circle']"
-            />
-          </transition>
-        </span>
-        <!-- temp activator that is not shown to disable tooltip -->
-      </template>
-      <v-card flat class="fixed-cell-renderer__card--hover">
-        <v-card-title>
-          <span class="title font-weight-light">{{ hoverTitle }}</span
-          ><v-spacer></v-spacer>
-        </v-card-title>
-        <v-card-text>
-          <span v-if="hoverText">{{ hoverText }} </span>
-          <span v-else v-html="hoverHtml"></span>
-        </v-card-text>
-      </v-card>
-    </v-tooltip>
+  <span @mouseleave="isHover = false" @mouseover="enableHoverOver">
+    <span :class="cell.cssClasses">{{ cell.value }} &nbsp;</span>
+    <span v-if="canHover">
+      <transition name="fixed-cell-renderer__animation" mode="out-in">
+        <tooltip :cell="cell" v-show="isHover"> ></tooltip>
+      </transition>
+    </span>
+    <!-- temp activator that is not shown to disable tooltip -->
   </span>
 </template>
 
@@ -34,9 +16,11 @@ import { Prop } from 'vue-property-decorator'
 
 import ComponentBase from '@vuescape/infrastructure/ComponentBase'
 
+const Tooltip = () => import(/* webpackChunkName: 'tool-tip' */ '@vuescape/components/Tooltip/').then(m => m.default)
+
 import { TreeTableItem } from './TreeTableItem'
 
-@Component({})
+@Component({ components: { Tooltip } })
 export default class FixedCellRenderer extends ComponentBase {
   @Prop({ type: Object, required: true })
   private cell: TreeTableItem
@@ -58,12 +42,19 @@ export default class FixedCellRenderer extends ComponentBase {
   private get hoverHtml() {
     return this.cell.hover ? this.cell.hover.html : ''
   }
+
+  private xPosition = '50%'
+  private yPosition = '50%'
+
+  private enableHoverOver(e: any) {    
+    console.info(e)
+    this.xPosition = e.clientX + 'px'
+    this.yPosition = e.clientY + 'px'
+    this.isHover = true
+  }
 }
 </script>
 <style>
-.fixed-cell-renderer__card--hover {
-  max-width: 300px;
-}
 .fixed-cell-renderer__animation-enter-active,
 .fixed-cell-renderer__animation-leave-active {
   transition: opacity 0.3s;
