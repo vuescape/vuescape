f850d79bb6e5eb07f80fca0d638e298a4b25806c
randylevy <randy.levy@gmail.com>
2020-07-30 05:42:23 -0400

    Add ability to render HTML in a Cell

---
+++ src/components/TreeTable/HtmlCellRenderer.vue
@@ -0,0 +1,22 @@
+<template>
+  <span v-html="cellValue"></span>
+</template>
+
+<script <script lang="ts">
+import Component from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+import ComponentBase from '@vuescape/infrastructure/ComponentBase'
+
+import { TreeTableItem } from './TreeTableItem'
+
+@Component({})
+export default class HtmlCellRenderer extends ComponentBase {
+  @Prop({ type: Object, required: true })
+  private cell: TreeTableItem
+
+  private get cellValue() {
+    return this.cell.displayValue ? this.cell.displayValue : this.cell.value
+  }
+}
+</script>
