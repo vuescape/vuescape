3bf5cc40f03bb2e67a7cff7c0849b2ed89d4028a
randylevy <randy.levy@gmail.com>
2020-09-15 07:23:49 -0400

    Legend button height should lines up with vuescape button height

--- src/components/LegendTable/LegendTable.vue
+++ src/components/LegendTable/LegendTable.vue
@@ -53,12 +53,13 @@ export default class LegendTable extends Vue {}
 .legend-table__v-btn--style {
   border: 1px solid #dddddd !important;
   border-radius: 5px;
-  height: 24px;
-  width: 24px;
+  height: 26px;
+  width: 26px;
   padding-left: 8px;
   padding-right: 8px;
   margin-top: 6px;
   margin-right: 6px;
+  margin-bottom: -7px;
 }
 table.legend-table__table--legend thead td {
   border: 0px;
