2a7bf4d11f55d6b2d6cc84e83a8dd3a6d5f5b3f3
randylevy <randy.levy@gmail.com>
2021-01-11 04:43:01 -0500

    Add renderer for empty cell.  Allows to toggle display without changing data.

---
+++ src/components/TreeTable/EmptyCellRenderer.vue
@@ -0,0 +1,20 @@
+<template>
+  <span></span>
+</template>
+
+<script <script lang="ts">
+import Component from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+import { TreeTableItem } from '@vuescape/components/TreeTable'
+import ComponentBase from '@vuescape/infrastructure/ComponentBase'
+
+@Component({})
+export default class EmptyCellRenderer extends ComponentBase {
+  @Prop({ type: Object, required: true })
+  private cell: TreeTableItem
+
+  @Prop({ type: Boolean, default: false })
+  private isHovering: boolean
+}
+</script>
