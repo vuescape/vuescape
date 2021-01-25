c40b029061f9585ba894b9fd1dbc09dfff2bec97
randylevy <randy.levy@gmail.com>
2021-01-25 13:36:49 -0500

    Increase width and height of legend button to align with vuescape button

--- src/components/LegendTable/LegendTable.vue
+++ src/components/LegendTable/LegendTable.vue
@@ -65,8 +65,8 @@ export default class LegendTable extends Vue {
 .legend-table__v-btn--style {
   border: 1px solid #dddddd !important;
   border-radius: 5px;
-  height: 24px;
-  width: 24px;
+  height: 26px;
+  width: 26px;
   padding-left: 8px;
   padding-right: 8px;
   margin-top: 6px;
