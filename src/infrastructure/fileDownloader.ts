5eb0e67708c5517db1c2b2833590f2fdace9c050
randylevy <randy.levy@gmail.com>
2021-04-26 00:01:02 -0400

    Revert "Allow download of zero length file"
    This reverts commit 2c71e378dea545b027b6a4294ca547f6b6015732.

--- src/infrastructure/fileDownloader.ts
+++ src/infrastructure/fileDownloader.ts
@@ -1,12 +1,8 @@
 import FileSaver from 'file-saver'
 
 export function downloadFile(data: string, isBase64EncodedBinary: boolean, filename: string) {
-  data = data || ''
-  let downloadedData: Uint8Array | string = data || ''
-
   if (data) {
-    // Do we really need to add newlines when there is a non-zero text file?
-    downloadedData = data + '\n\n'
+    let downloadedData: Uint8Array | string = data + '\n\n'
     if (isBase64EncodedBinary) {
       const byteCharacters = atob(data)
       const byteNumbers = new Array(byteCharacters.length)
@@ -16,9 +12,7 @@ export function downloadFile(data: string, isBase64EncodedBinary: boolean, filen
       const byteArray = new Uint8Array(byteNumbers)
       downloadedData = new Uint8Array(byteArray)
     }
-  } else {
-    downloadedData = ''
+    const blob = new Blob([downloadedData], { type: 'application/octet-stream; charset=utf-8' })
+    FileSaver.saveAs(blob, filename)
   }
-  const blob = new Blob([downloadedData], { type: 'application/octet-stream; charset=utf-8' })
-  FileSaver.saveAs(blob, filename)
 }
