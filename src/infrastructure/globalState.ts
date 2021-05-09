519c7047becb51822e34bfed578e9d3eb6bc8e8a
randylevy <randy.levy@gmail.com>
2021-05-08 23:26:53 -0400

    Add comments

--- src/infrastructure/globalState.ts
+++ src/infrastructure/globalState.ts
@@ -1,3 +1,10 @@
+/**
+ * Registers a global variable. For the web this would be attaching the variable to window.
+ * @param name The name of the variable.
+ * @param value The value.
+ * @param shouldAlwaysRegister Whether to only always register or only register when variable name is undefined 
+ * @returns void
+ */
 export function registerGlobal(name: string, value: any, shouldAlwaysRegister = false) {
   const win = window as any
   if (shouldAlwaysRegister) {
