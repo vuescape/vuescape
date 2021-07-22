fff0f90e46fff170143b53dcfee57cab22a13351
randylevy <randy.levy@gmail.com>
2021-07-22 14:45:25 -0400

    Fix bug with result being 0 and being falsey

--- src/infrastructure/converters.ts
+++ src/infrastructure/converters.ts
@@ -1,13 +1,16 @@
 export function toEnum<T>(enumType: T, enumString: string): T[keyof T] {
   // Split to handle flags
-  const enumStrings = enumString.split('|')
-  let result: T[keyof T] | null = null
+  const enumStrings = enumString
+    .split('|')
+    .map(_ => shouldCapitalizeFirstCharacter ? _.replace(/^\w/, c => c.toUpperCase()) : _)
+  
+    let result: T[keyof T] | null = null
 
   for (const enumStringKey of enumStrings) {
     const enumKey = enumStringKey as keyof T
     const enumValue = enumType[enumKey]
 
-    if (!result) {
+    if (result == null) {
       result = enumValue
     } else {
       // Add value if flags
@@ -18,7 +21,7 @@ export function toEnum<T>(enumType: T, enumString: string): T[keyof T] {
 
   // Must have a result now unless we couldn't find the enum. Don't have the enum name here
   // so just list out the values (should be able to determine the enum from those in the case of troubleshooting).
-  if (!result) {
+  if (result == null) {
     throw new Error(
       `The enum string, '${enumString}', was not found in the enum with values of: '${Object.values(enumType).filter(
         v => isNaN(Number(v)),
