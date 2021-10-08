81b22611d18bfda7ab1e5f7787e85aeff82e90e5
randylevy <randy.levy@gmail.com>
2021-10-08 13:51:52 -0400

    Add comments and empty GUID property

--- src/types/Guid.ts
+++ src/types/Guid.ts
@@ -5,6 +5,8 @@ export class Guid {
       d += performance.now()
     }
     // Inspired by https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+    // There are chances of collision here so not recommended for truly unique identifiers across an entire 
+    // application/system. Typical use is for temporary IDs generated in the browser.
     // tslint:disable:no-bitwise
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
       const r = (d + Math.random() * 16) % 16 | 0
@@ -12,5 +14,9 @@ export class Guid {
       return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16)
     })
     // tslint:enable:no-bitwise
+  }
+
+  public static get empty() {
+    return '00000000-0000-0000-0000-000000000000'
   }
 }
