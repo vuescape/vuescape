b2c6138fc22b0aa4bdfb571b60664dcff1fa62a6
randylevy <randy.levy@gmail.com>
2022-02-17 03:43:08 -0500

    Only sort on the last header row

--- src/reporting-domain/Logic/TreeTableHeaderRowHelpers.ts
+++ src/reporting-domain/Logic/TreeTableHeaderRowHelpers.ts
@@ -3,14 +3,17 @@ import { SortDirection, TreeTableHeaderCell, TreeTableHeaderRow } from '../TreeT
 export function getSortedHeaderCellWithIndex(headers: Array<TreeTableHeaderRow>) {
   let sortHeader: Array<{ cell: TreeTableHeaderCell; index: number }>
 
-  for (const row of headers) {
-    sortHeader = row.cells
-      .map((cell: TreeTableHeaderCell, index: number) => {
-        const result = { cell, index }
-        return result
-      })
-      .filter(_ => _.cell.columnSorter && _.cell.columnSorter.sortDirection !== SortDirection.None)
-
-    return sortHeader[0]
+  if (!headers || !headers.length)  {
+    return
   }
+
+  const row = headers[headers.length - 1]
+  sortHeader = row.cells
+    .map((cell: TreeTableHeaderCell, index: number) => {
+      const result = { cell, index }
+      return result
+    })
+    .filter(_ => _.cell.columnSorter && _.cell.columnSorter.sortDirection !== SortDirection.None)
+
+  return sortHeader[0]
 }
