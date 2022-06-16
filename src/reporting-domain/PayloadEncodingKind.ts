5ced3a58cde1a410020a9cbab33eeb05aea443e9
randylevy <randy.levy@gmail.com>
2022-06-16 03:50:21 -0400

    Add enum

---
+++ src/reporting-domain/PayloadEncodingKind.ts
@@ -0,0 +1,11 @@
+export enum PayloadEncodingKind {
+  /// <summary>
+  /// No encoding. Suitable for text.
+  /// </summary>
+  None = 0,
+
+  /// <summary>
+  /// Base64 encoding.
+  /// </summary>
+  Base64,
+}
