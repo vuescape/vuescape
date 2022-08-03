571a2f878a4798ff70f6a942f8cea5d7b43b08ba
randylevy <randy.levy@gmail.com>
2022-08-03 14:14:35 -0400

    Constrain CSS selector to avoid conflicts with TreeTable-v2

--- src/components/TreeTable/TreeTable.vue
+++ src/components/TreeTable/TreeTable.vue
@@ -1,9 +1,5 @@
 <template>
-  <div
-    :key="id"
-    class="tree-table__div--box"
-    :style="cssStyleValue"
-  >
+  <div :key="id" class="tree-table__div--box" :style="cssStyleValue">
     <vue-scrolling-table
       :scroll-horizontal="shouldScrollHorizontalValue"
       :scroll-vertical="shouldScrollVerticalValue"
@@ -14,17 +10,8 @@
       :class="{ freezeFirstColumn: shouldFreezeFirstColumnValue }"
     >
       <template slot="thead">
-        <tr
-          v-for="headerRow in headersToDisplay"
-          :class="headerRow.cssClasses"
-          :key="headerRow.id"
-        >
-          <th
-            v-for="header in headerRow.cells"
-            :class="header.cssClasses"
-            :key="header.id"
-            :colspan="header.colspan"
-          >
+        <tr v-for="headerRow in headersToDisplay" :class="headerRow.cssClasses" :key="headerRow.id">
+          <th v-for="header in headerRow.cells" :class="header.cssClasses" :key="header.id" :colspan="header.colspan">
             <component
               :is="header.renderer || 'HeaderCellRenderer'"
               :header="header"
@@ -34,11 +21,7 @@
         </tr>
       </template>
       <template slot="tbody">
-        <row-renderer
-          v-for="row in rowsToDisplay"
-          :key="row.id"
-          :row="row"
-        ></row-renderer>
+        <row-renderer v-for="row in rowsToDisplay" :key="row.id" :row="row"></row-renderer>
       </template>
     </vue-scrolling-table>
   </div>
@@ -106,10 +89,8 @@ export default class TreeTable extends ComponentBase {
   @Prop({ type: Function, required: false })
   private treeTableSorter?: (rows: Array<TreeTableRow>, headers: Array<TreeTableHeaderRow>) => Array<TreeTableRow>
 
-  private treeTableSorterImpl: (
-    rows: Array<TreeTableRow>,
-    headers: Array<TreeTableHeaderRow>,
-  ) => Array<TreeTableRow> = this.defaultTreeTableSorter
+  private treeTableSorterImpl: (rows: Array<TreeTableRow>, headers: Array<TreeTableHeaderRow>) => Array<TreeTableRow> =
+    this.defaultTreeTableSorter
 
   private rowsToDisplay: Array<TreeTableRow> = []
 
@@ -229,22 +210,26 @@ export default class TreeTable extends ComponentBase {
 // https://stackoverflow.com/questions/41882616/why-border-is-not-visible-with-position-sticky-when-background-color-exists
 </script>
 <style>
-.fixed-column {
+div.tree-table__div--box table.scrolling {
+  border-collapse: separate;
+  border-spacing: 0;
+}
+div.tree-table__div--box .fixed-column {
   text-align: unset;
 }
-.tree-table-item__td--clickable {
+div.tree-table__div--box .tree-table-item__td--clickable {
   cursor: pointer;
 }
-table.scrolling tr {
+div.tree-table__div--box table.scrolling tr {
   text-align: left;
 }
 /* TODO: fix this so that we don't get double lines*/
-table.scrolling tr.subheader {
+div.tree-table__div--box table.scrolling tr.subheader {
   /* margin: -1px 0; */
   border-bottom: 1px solid #555555 !important;
   border-top: 1px solid #555555 !important;
 }
-table.scrolling td.subheader {
+div.tree-table__div--box table.scrolling td.subheader {
   background-color: #f8f8f8 !important;
   /* border-bottom: 1px solid #555555 !important;
   border-top: 1px solid #555555 !important;
@@ -253,29 +238,29 @@ table.scrolling td.subheader {
   height: 33px;
   vertical-align: middle;
 }
-table.scrolling tr td.focused-metric {
+div.tree-table__div--box table.scrolling tr td.focused-metric {
   background-color: rgb(235, 248, 240) !important;
 }
-table.scrolling tr.data-row:hover td {
+div.tree-table__div--box table.scrolling tr.data-row:hover td {
   background-color: rgb(235, 248, 240) !important;
 }
-table.scrolling td i.material-icons {
+div.tree-table__div--box table.scrolling td i.material-icons {
   vertical-align: bottom;
 }
-table.scrolling td,
-table.scrolling th {
+div.tree-table__div--box table.scrolling td,
+div.tree-table__div--box table.scrolling th {
   border: 0px solid #ddd !important;
   font-size: small;
 }
-table.scrolling td {
+div.tree-table__div--box table.scrolling td {
   height: 31px;
   vertical-align: middle;
 }
-table.scrolling thead.scrollsync {
+div.tree-table__div--box table.scrolling thead.scrollsync {
   background-color: white !important;
   /* overflow-y: auto !important; Pushes header over scrollbar which causes misalignment of header and body when scroll bar is visible at the far right */
 }
-table.scrolling thead th {
+div.tree-table__div--box table.scrolling thead th {
   border: 0px;
   background-color: #16a5c6 !important;
   color: #ffffff;
@@ -283,16 +268,16 @@ table.scrolling thead th {
   height: 32px;
   font-weight: 500;
 }
-table.scrolling td.cell--value--raw.subheader {
+div.tree-table__div--box table.scrolling td.cell--value--raw.subheader {
   border-left: 0 !important;
 }
-table.scrolling tr.cell--value--grid-line {
+div.tree-table__div--box table.scrolling tr.cell--value--grid-line {
   border-bottom: 1px solid #ddd !important;
 }
-table.scrolling td.cell--value--grid-line {
+div.tree-table__div--box table.scrolling td.cell--value--grid-line {
   /* border-bottom: 1px solid #ddd!important; */
 }
-table.scrolling td.cell--value--raw {
+div.tree-table__div--box table.scrolling td.cell--value--raw {
   width: 13em;
   max-width: 13em;
   min-width: 13em;
@@ -301,7 +286,7 @@ table.scrolling td.cell--value--raw {
   padding-left: 4px;
   border-left: 1px solid #ddd !important;
 }
-table.scrolling td.cell--value--common {
+div.tree-table__div--box table.scrolling td.cell--value--common {
   width: 7em;
   max-width: 7em;
   min-width: 7em;
@@ -310,7 +295,7 @@ table.scrolling td.cell--value--common {
   border-left: 0 !important;
   text-align: center;
 }
-table.scrolling td {
+div.tree-table__div--box table.scrolling td {
   border: 0px;
   white-space: nowrap !important;
   border-left: 1px solid #dddddd;
@@ -318,25 +303,25 @@ table.scrolling td {
   border-top: 0;
   border-bottom: 0;
 }
-table.scrolling .w2 {
+div.tree-table__div--box table.scrolling .w2 {
   width: 20em;
   min-width: 20em;
   max-width: 20em;
 }
-table.scrolling tfoot tr th {
+div.tree-table__div--box table.scrolling tfoot tr th {
   width: 130em;
   min-width: 130em;
   max-width: 130em;
 }
-table.freezeFirstColumn thead tr,
-table.freezeFirstColumn tbody tr {
+div.tree-table__div--box table.freezeFirstColumn thead tr,
+div.tree-table__div--box table.freezeFirstColumn tbody tr {
   display: block;
   width: min-content;
 }
-table.freezeFirstColumn thead td:first-child,
-table.freezeFirstColumn tbody td:first-child,
-table.freezeFirstColumn thead th:first-child,
-table.freezeFirstColumn tbody th:first-child {
+div.tree-table__div--box table.freezeFirstColumn thead td:first-child,
+div.tree-table__div--box table.freezeFirstColumn tbody td:first-child,
+div.tree-table__div--box table.freezeFirstColumn thead th:first-child,
+div.tree-table__div--box table.freezeFirstColumn tbody th:first-child {
   position: sticky;
   position: -webkit-sticky;
   left: 0;
@@ -344,45 +329,45 @@ table.freezeFirstColumn tbody th:first-child {
   background-clip: padding-box;
 }
 
-table.scrolling {
+div.tree-table__div--box table.scrolling {
   background-color: white !important;
   height: 100% !important;
 }
-.tree-table__div--box {
+div.tree-table__div--box .tree-table__div--box {
   clear: both;
   padding: 0;
   margin-left: auto;
   margin-right: auto;
   overflow: hidden;
 }
-table.scrolling.scrollx tbody {
+div.tree-table__div--box table.scrolling.scrollx tbody {
   overflow-x: auto !important;
 }
-table.scrolling.scrolly tbody {
+div.tree-table__div--box table.scrolling.scrolly tbody {
   overflow-y: auto !important;
 }
 
-td.selected-metric-left {
+div.tree-table__div--box td.selected-metric-left {
   -webkit-box-shadow: inset 3px 0 0px 0px #666, inset 0 3px 0 0 #666, inset 0 -3px 0 0 #666;
   -moz-box-shadow: inset 3px 0 0px 0px #666, inset 0 3px 0 0 #666, inset 0 -3px 0 0 #666;
   box-shadow: inset 3px 0 0px 0px #666, inset 0 3px 0 0 #666, inset 0 -3px 0 0 #666;
 }
 
-td.selected-metric-right {
+div.tree-table__div--box td.selected-metric-right {
   -webkit-box-shadow: inset -3px 0 0px 0px #666, inset 0 3px 0 0 #666, inset 0 -3px 0 0 #666;
   -moz-box-shadow: inset -3px 0 0px 0px #666, inset 0 3px 0 0 #666, inset 0 -3px 0 0 #666;
   box-shadow: inset -3px 0 0px 0px #666, inset 0 3px 0 0 #666, inset 0 -3px 0 0 #666;
   /* padding: 0px !important; */
 }
 
-td.selected-metric-interior {
+div.tree-table__div--box td.selected-metric-interior {
   -webkit-box-shadow: inset 0 3px 0 0 #666, inset 0 -3px 0 0 #666;
   -moz-box-shadow: inset 0 3px 0 0 #666, inset 0 -3px 0 0 #666;
   box-shadow: inset 0 3px 0 0 #666, inset 0 -3px 0 0 #666;
   /* padding: 0px !important; */
 }
 
-td.selected-metric {
+div.tree-table__div--box td.selected-metric {
   cursor: default;
 }
 </style>
