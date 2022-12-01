e0ba76ec593b7d51e712072620b4079d488753e8
randylevy <randy.levy@gmail.com>
2022-12-01 00:29:29 -0500

    Add ability to specify an image as a menu option (e.g. Home corporate icon) and an override for determining active menu item using a route path

--- src/types/Menu.ts
+++ src/types/Menu.ts
@@ -12,4 +12,13 @@ export interface Menu {
   isDivider?: boolean
   horizontalAlignment?: HorizontalAlignment
   shouldFireClickEvent?: boolean
+  
+  // Set an image src
+  imageSrc? : string
+
+  // This is not used for navigation.
+  // Override path to use if this item should be considered active
+  // when the actual route in the browser differs from the Menu path.
+  // If not specified the path is used to determine if a route is active.
+  pathIsActiveWhen?: string
 }
