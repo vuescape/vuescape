66837dd477afb3328d783373f2d45b5ff084c376
randylevy <randy.levy@gmail.com>
2023-01-11 02:34:16 -0500

    Formatting

--- src/components/StepWizard/StepWizard.vue
+++ src/components/StepWizard/StepWizard.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="step-wizard__stepper-box">
-    <div v-if="shouldShowProgressBarValue" class="top">
+    <div
+      v-if="shouldShowProgressBarValue"
+      class="top"
+    >
       <div class="steps-wrapper">
         <template v-if="shouldShowTopButtons">
-          <div v-if="currentStepIndex > 0" class="stepper-button-top previous" @click="backStep()">
+          <div
+            v-if="currentStepIndex > 0"
+            class="stepper-button-top previous"
+            @click="backStep()"
+          >
             <font-awesome-icon :icon="['fad', 'arrow-alt-circle-left']" />
           </div>
         </template>
         <template v-for="(step, index) in steps">
-          <div :class="['step', getStepStatus(index, step)]" :key="index" :style="{ width: `${100 / steps.length}%` }">
-            <div class="circle" :class="{ 'divider-line': index !== steps.length - 1 }">
+          <div
+            :key="index"
+            :class="['step', getStepStatus(index, step)]"
+            :style="{ width: `${100 / steps.length}%` }"
+          >
+            <div
+              :class="{ 'divider-line': index !== steps.length - 1 }"
+              class="circle"
+            >
               <!-- <div class="divider-line" :style="{ width: `${(100 / steps.length) * (steps.length - 1) - 10}%` }"></div> -->
               <!-- TODO: -->
               <!-- <font-awesome-icon :icon="['fal', 'chevron-down']" /> -->
@@ -32,65 +46,90 @@
         </div>
       </div>
     </div>
-    <div v-else style="height: 63.66px;"></div>
-    <div class="content" v-loading="isSpinning">
-      <transition :name="enterAnimation" mode="out-in">
+    <div
+      v-else
+      style="height: 63.66px;"
+    ></div>
+    <div
+      v-loading="isSpinning"
+      class="content"
+    >
+      <transition
+        :name="enterAnimation"
+        mode="out-in"
+      >
         <!--If keep alive-->
         <keep-alive v-if="steps[currentStepIndex] && steps[currentStepIndex].shouldKeepComponentAlive">
           <component
-            :key="steps[currentStepIndex].name + currentStepIndex"
             :is="steps[currentStepIndex].component"
+            :key="steps[currentStepIndex].name + currentStepIndex"
             :clickedNext="nextButton[currentStepIndex]"
+            :current-step="steps[currentStepIndex]"
+            v-bind="steps[currentStepIndex].props"
             @can-continue="proceed"
             @component-activated="componentActivated"
             @change-next="changeNextBtnValue"
-            :current-step="steps[currentStepIndex]"
-            v-bind="steps[currentStepIndex].props"
           ></component>
         </keep-alive>
         <!--If not show component and destroy it in each step change. Instead of v-else use another v-if with the opposite logic to avoid a script error.-->
         <component
+          :is="steps[currentStepIndex].component"
           v-if="steps[currentStepIndex] && !steps[currentStepIndex].shouldKeepComponentAlive"
           :key="steps[currentStepIndex].name + currentStepIndex"
-          :is="steps[currentStepIndex].component"
           :clickedNext="nextButton[currentStepIndex]"
-          @can-continue="proceed"
-          @change-next="changeNextBtnValue"
           :current-step="steps[currentStepIndex]"
           v-bind="steps[currentStepIndex].props"
+          @can-continue="proceed"
+          @change-next="changeNextBtnValue"
         ></component>
       </transition>
     </div>
     <v-layout justify-center>
-      <v-container fluid grid-list-md>
-        <v-layout row justify-center>
+      <v-container
+        fluid
+        grid-list-md
+      >
+        <v-layout
+          justify-center
+          row
+        >
           <v-flex xs1></v-flex>
-          <v-flex xs10 style="text-align: center; font-size: 22px; font-weight: 500;">
+          <v-flex
+            style="text-align: center; font-size: 22px; font-weight: 500;"
+            xs10
+          >
             <div :class="['bottom', currentStepIndex > 0 ? '' : '']">
               <!-- only-next -->
               <div class="stepper-button previous">
                 <vuescape-button
                   v-if="currentStepIndex !== 0"
-                  :icons="['fad', 'arrow-alt-circle-left']"
                   :disabled="false"
+                  :icons="['fad', 'arrow-alt-circle-left']"
                   Depressed
                   @click="backStep()"
                 >
                   &nbsp;Back
                 </vuescape-button>
-                <span v-if="cancelRouteOrCallback" style="margin-top: 7px;">
-                  <v-btn class="cancel" flat @click="cancel">Cancel</v-btn>
+                <span
+                  v-if="cancelRouteOrCallback"
+                  style="margin-top: 7px;"
+                >
+                  <v-btn
+                    class="cancel"
+                    flat
+                    @click="cancel"
+                  >Cancel</v-btn>
                   <!-- <a class="cancel" @click="cancel">Cancel</a> -->
                 </span>
               </div>
               <div class="stepper-button next">
                 <vuescape-button
-                  :isDisabled="!canContinue"
-                  :icons="isFinalStep ? ['fad', 'arrow-alt-circle-right'] : ['fad', 'arrow-alt-circle-right']"
-                  Depressed
-                  @click="nextStep()"
-                  iconPosition="after"
                   :class="{ finalStep: isFinalStep }"
+                  :icons="isFinalStep ? ['fad', 'arrow-alt-circle-right'] : ['fad', 'arrow-alt-circle-right']"
+                  :isDisabled="!canContinue"
+                  Depressed
+                  iconPosition="after"
+                  @click="nextStep()"
                 >
                   {{ isFinalStep ? finalStepButtonTextValue : 'Next' }}&nbsp;
                 </vuescape-button>
@@ -106,36 +145,46 @@
 <script lang="ts">
 import Vue from 'vue'
 import { Component, Prop, Watch } from 'vue-property-decorator'
-
-const VuescapeButton = () =>
-  import(/* webpackChunkName: 'vuescape-button' */ '@vuescape/components/VuescapeButton/').then(m => m.default)
-
 import { Step } from './Step'
+
+const VuescapeButton = () => import(/* webpackChunkName: 'vuescape-button' */ '@vuescape/components/VuescapeButton/').then(
+  m => m.default)
 
 @Component({
   components: { VuescapeButton },
 })
 export default class StepWizard extends Vue {
   private isSpinning = false
-  @Prop({ type: Boolean, default: true })
-  private shouldShowTopButtons: boolean
-  @Prop({ type: Boolean, default: true })
-  private shouldShowProgressBar: boolean
-  @Prop({ type: String, default: 'Finish' })
-  private finalStepButtonText: string
-  @Prop({ type: Array, required: false, default: () => [] })
-  private wizardSteps: Array<Step>
-  @Prop({ type: [String, Function], required: false })
-  private cancelRouteOrCallback: any
+  @Prop({
+    type   : Boolean,
+    default: true,
+  }) private shouldShowTopButtons: boolean
+  @Prop({
+    type   : Boolean,
+    default: true,
+  }) private shouldShowProgressBar: boolean
+  @Prop({
+    type   : String,
+    default: 'Finish',
+  }) private finalStepButtonText: string
+  @Prop({
+    type    : Array,
+    required: false,
+    default : () => [],
+  }) private wizardSteps: Array<Step>
+  @Prop({
+    type    : [String, Function],
+    required: false,
+  }) private cancelRouteOrCallback: any
 
   private shouldShowProgressBarValue = true
-  private finalStepButtonTextValue = ''
+  private finalStepButtonTextValue   = ''
 
-  private currentStepIndex: number = 0
-  private previousStepIndex: number = 0
+  private currentStepIndex: number   = 0
+  private previousStepIndex: number  = 0
   private nextButton: Array<boolean> = []
-  private canContinue = false
-  private steps: Array<Step> = []
+  private canContinue                = false
+  private steps: Array<Step>         = []
 
   private get activeStep() {
     const result = this.steps.filter((_: Step, index: number) => index === this.currentStepIndex)
@@ -155,7 +204,8 @@ export default class StepWizard extends Vue {
     return 'step-wizard__animation'
     if (this.currentStepIndex < this.previousStepIndex) {
       return 'step-wizard__animation-slide-out'
-    } else {
+    }
+    else {
       return 'step-wizard__animation-slide-in'
     }
   }
@@ -164,7 +214,8 @@ export default class StepWizard extends Vue {
     return 'step-wizard__animation'
     if (this.currentStepIndex > this.previousStepIndex) {
       return 'step-wizard__animation-slide-in'
-    } else {
+    }
+    else {
       return 'step-wizard__animation-slide-out'
     }
   }
@@ -181,11 +232,11 @@ export default class StepWizard extends Vue {
 
   @Watch('wizardSteps')
   private onStepsChanged(newValue: Array<Step>, oldValue: Array<Step>) {
-    this.nextButton = []
-    this.currentStepIndex = 0
+    this.nextButton        = []
+    this.currentStepIndex  = 0
     this.previousStepIndex = 0
-    this.canContinue = false
-    this.steps = newValue
+    this.canContinue       = false
+    this.steps             = newValue
     this.activateStep(0)
   }
 
@@ -202,7 +253,7 @@ export default class StepWizard extends Vue {
   private activateStep(index: number, back = false) {
     if (this.steps[index]) {
       this.previousStepIndex = this.currentStepIndex
-      this.currentStepIndex = index
+      this.currentStepIndex  = index
       if (!back) {
         this.$emit('completed-step', this.previousStepIndex)
       }
@@ -221,7 +272,7 @@ export default class StepWizard extends Vue {
   }
 
   private async nextStepAction() {
-    this.canContinue = true
+    this.canContinue                       = true
     this.nextButton[this.currentStepIndex] = true
     if (this.canContinue) {
       if (this.steps[this.currentStepIndex].shouldDisplayLoadingOnNext) {
@@ -273,9 +324,9 @@ export default class StepWizard extends Vue {
 
   private init() {
     // Initiate stepper
-    this.finalStepButtonTextValue = this.finalStepButtonText
+    this.finalStepButtonTextValue   = this.finalStepButtonText
     this.shouldShowProgressBarValue = this.shouldShowProgressBar
-    this.steps = this.wizardSteps
+    this.steps                      = this.wizardSteps
     this.activateStep(0)
   }
 
@@ -298,7 +349,7 @@ export default class StepWizard extends Vue {
   opacity: 0;
 }
 /* .step-wizard__animation-slide-in {
-  /* transition: opacity .3s ease-in; 
+  /* transition: opacity .3s ease-in;
   transform: translateX(-900px);
 }
 */
@@ -308,37 +359,37 @@ export default class StepWizard extends Vue {
 .step-wizard__stepper-box {
   background-color: white;
   /* box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24); */
-  min-height: 200px;
+  min-height:       200px;
 }
 .step-wizard__stepper-box .top {
-  display: flex;
-  align-items: center;
-  position: relative;
+  display:         flex;
+  align-items:     center;
+  position:        relative;
   justify-content: center;
 }
 .step-wizard__stepper-box .top .stepper-button-top {
-  z-index: 20;
-  padding: 0.4rem;
-  border-radius: 100rem;
+  z-index:         20;
+  padding:         0.4rem;
+  border-radius:   100rem;
   /* cursor: pointer; */
-  position: absolute;
-  display: flex;
-  align-items: center;
+  position:        absolute;
+  display:         flex;
+  align-items:     center;
   justify-content: space-between;
 }
 .step-wizard__stepper-box .top .stepper-button-top.next {
   border: 2px solid #3383c8;
-  color: #3383c8;
-  right: 1%;
+  color:  #3383c8;
+  right:  1%;
 }
 .step-wizard__stepper-box .top .stepper-button-top.next.deactivated {
   border: 2px solid #ccc !important;
-  color: #ccc;
+  color:  #ccc;
   cursor: not-allowed !important;
 }
 .step-wizard__stepper-box .top .stepper-button-top.previous {
   color: #333;
-  left: 1%;
+  left:  1%;
 }
 /* .step-wizard__stepper-box .top .divider-line {
   border-bottom: 2px solid #ccc;
@@ -368,50 +419,50 @@ export default class StepWizard extends Vue {
   }
 } */
 .step-wizard__stepper-box .top .steps-wrapper {
-  display: flex;
-  align-items: center;
+  display:         flex;
+  align-items:     center;
   justify-content: space-between;
-  position: relative;
-  width: 95%;
-  left: 0;
-  padding: 2% 0%;
+  position:        relative;
+  width:           95%;
+  left:            0;
+  padding:         2% 0%;
 }
 .step-wizard__stepper-box .top .steps-wrapper .step {
-  position: relative;
-  display: flex;
+  position:       relative;
+  display:        flex;
   flex-direction: column;
-  align-items: center;
-  text-align: center;
+  align-items:    center;
+  text-align:     center;
 }
 .step-wizard__stepper-box .top .steps-wrapper .step.completed .circle.divider-line::before {
   background-position: left bottom;
 }
 .step-wizard__stepper-box .top .steps-wrapper .step .circle.divider-line::before {
-  content: '';
-  display: inline-block;
-  height: 2px;
-  position: absolute;
-  width: calc(100% - 1rem - 4px);
-  margin-left: calc(0.5rem + 2px);
-  background: linear-gradient(to right, #16a5c6 50%, #ccc 50%);
-  background-size: 200% 100%;
+  content:             '';
+  display:             inline-block;
+  height:              2px;
+  position:            absolute;
+  width:               calc(100% - 1rem - 4px);
+  margin-left:         calc(0.5rem + 2px);
+  background:          linear-gradient(to right, #16a5c6 50%, #ccc 50%);
+  background-size:     200% 100%;
   background-position: right bottom;
-  transition: all 600ms ease;
+  transition:          all 600ms ease;
 }
 .step-wizard__stepper-box .top .steps-wrapper .step .circle {
-  border-style: solid;
-  border-width: 2px;
-  padding: 0 1rem;
+  border-style:     solid;
+  border-width:     2px;
+  padding:          0 1rem;
   background-color: white;
-  border-radius: 100rem;
-  padding: 0.5rem;
+  border-radius:    100rem;
+  padding:          0.5rem;
 }
 
 .step-wizard__stepper-box .top .steps-wrapper .step.deactivated .circle {
   border-color: #bbb;
 }
 .step-wizard__stepper-box .top .steps-wrapper .step.completed .circle {
-  border-color: #16a5c6;
+  border-color:     #16a5c6;
   background-color: #16a5c6;
 }
 .step-wizard__stepper-box .top .steps-wrapper .step.completed .circle::after {
@@ -425,53 +476,53 @@ export default class StepWizard extends Vue {
 }
 .step-wizard__stepper-box .top .steps-wrapper .step .step-title {
   position: absolute;
-  top: 90%;
-  width: 100%;
+  top:      90%;
+  width:    100%;
 }
 .step-wizard__stepper-box .top .steps-wrapper .step .step-title h1,
 .step-wizard__stepper-box .top .steps-wrapper .step .step-title h2,
 .step-wizard__stepper-box .top .steps-wrapper .step .step-title h3,
 .step-wizard__stepper-box .top .steps-wrapper .step .step-title h4,
 .step-wizard__stepper-box .top .steps-wrapper .step .step-title h5 {
-  margin: 0 0 0.2rem 0;
-  color: #333;
+  margin:      0 0 0.2rem 0;
+  color:       #333;
   font-weight: bold;
 }
 .step-wizard__stepper-box .top .steps-wrapper .step .step-title .step-subtitle {
   font-weight: lighter;
-  margin: 0;
-  color: #555;
+  margin:      0;
+  color:       #555;
 }
 .step-wizard__stepper-box .content {
   overflow: hidden;
-  margin: 2rem 0;
+  margin:   2rem 0;
 }
 .step-wizard__stepper-box .bottom {
-  position: fixed;
-  bottom: 40px;
+  position:        fixed;
+  bottom:          40px;
   justify-content: space-between;
-  align-items: center;
-  padding-top: 2rem;
-  padding-top: 2rem;
-  padding-right: 0;
-  padding-left: 0;
+  align-items:     center;
+  padding-top:     2rem;
+  padding-top:     2rem;
+  padding-right:   0;
+  padding-left:    0;
 }
 .step-wizard__stepper-box .bottom.only-next {
   right: calc(10vh);
 }
 .step-wizard__stepper-box .bottom .stepper-button {
-  padding-top: 0.5rem;
-  padding-bottom: 0.5rem;
-  padding-left: 0;
-  padding-right: 0;
-  display: flex;
-  align-items: center;
+  padding-top:     0.5rem;
+  padding-bottom:  0.5rem;
+  padding-left:    0;
+  padding-right:   0;
+  display:         flex;
+  align-items:     center;
   justify-content: space-between;
 }
 .step-wizard__stepper-box .bottom .stepper-button.next {
-  right: calc(10vh);
+  right:    calc(10vh);
   position: fixed;
-  bottom: 40px;
+  bottom:   40px;
 }
 /*
 .step-wizard__stepper-box .bottom .stepper-button.next.deactivated {
@@ -481,20 +532,20 @@ export default class StepWizard extends Vue {
 */
 .step-wizard__stepper-box .bottom .stepper-button.previous {
   position: fixed;
-  bottom: 40px;
-  left: calc(10vh);
+  bottom:   40px;
+  left:     calc(10vh);
 }
 .step-wizard__stepper-box .bottom .vuescape-button__v-btn--style {
-  height: 40px;
+  height:    40px;
   font-size: 16px;
 }
 .step-wizard__stepper-box .bottom .cancel {
   text-decoration: underline;
-  font-size: 16px;
-  height: 40px;
-  left: calc(100% / 2 - 50px);
-  position: fixed;
-  bottom: 40px;
+  font-size:       16px;
+  height:          40px;
+  left:            calc(100% / 2 - 50px);
+  position:        fixed;
+  bottom:          40px;
 }
 .step-wizard__stepper-box .bottom .vuescape-button__v-btn--style {
   border-color: #9bdddb !important;
@@ -502,16 +553,16 @@ export default class StepWizard extends Vue {
   color: white !important; */
 }
 .step-wizard__stepper-box .bottom .next .vuescape-button__v-btn--style.v-btn--disabled {
-  color: unset !important;
+  color:            unset !important;
   background-color: unset !important;
   /* border-color: #16a5c6 !important;
   background-color: unset !important;
   color: unset!important; */
 }
 .step-wizard__stepper-box .bottom .next .vuescape-button__v-btn--style {
-  border-color: #9bdddb !important;
+  border-color:     #9bdddb !important;
   background-color: #16a5c6 !important;
-  color: white !important;
+  color:            white !important;
   /* background-color: unset !important;
   color: unset!important; */
 }
